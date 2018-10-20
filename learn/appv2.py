b3f66cba77f579b412c54bf50f5de2bbc39e3c4f
yamauchi-kotaro <t160161@mail.ryukoku.ac.jp>
2018-10-20 13:25:25 +0900

    mod app2

--- learn/appv2.py
+++ learn/appv2.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
+import numpy as np
+from PIL import Image
+import io
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False #日本語文字化け対策
 app.config["JSON_SORT_KEYS"] = False #ソートをそのまま
 
 
-@app.route('/good')
+@app.route('/good',methods=['POST'])
 def good():
-    import main
-    name = "Good"
-    z = main.color_detection('kondate.jpg')
-    return z
+    #import main
+    img_data = request.files['bmp'].read()
+    img = Image.open(io.BytesIO(img_data))
+    img.save('test.jpg')
+
+    #z = main.color_detection('kondate.jpg')
+    name = request.form["name"]
+    return 'Good' + name
 
 
 ## おまじない
